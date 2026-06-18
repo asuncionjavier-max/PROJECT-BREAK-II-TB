d6561962c5490c20d9c8b5116eb6b094b17c26dc
Javi <asuncionjavier96@gmail.com>
2026-06-18 18:09:08 +0200

    >feat: añadimos app server instalamos express y nos aseguramos de que funciona

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./app.js"
+const PORT = 3000;
+
+app.listen(PORT, () =>{
+    try {
+        console.log(`Servidor funcionando en puerto${PORT} `)
+        
+    } catch (error) {
+        
+        
+    }
+})
