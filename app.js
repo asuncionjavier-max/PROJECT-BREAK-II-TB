be50769576dc12be9b7711bed6a4a9b517e6734e
Javi <asuncionjavier96@gmail.com>
2026-07-05 22:57:23 +0200

    feat: añadimos swagger y documentacion

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ import express from "express";
 import indexRoutes from "./src/routes/indexroutes.js";
 import { errorHandler } from "./src/middlewares/errorhandler.js";
 import cookieParser from "cookie-parser";
+import swaggerUi from "swagger-ui-express"
+import fs from "node:fs"
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(join(__dirname, "./swagger.json"),"utf8"),
+)
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use("/api", indexRoutes);
 
 app.use(errorHandler);
